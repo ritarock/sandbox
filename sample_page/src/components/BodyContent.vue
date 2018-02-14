89757b7b053f22318036e836b276d53402b38007
ritarock <dvryz1204@gmail.com>
2018-02-14 22:23:48 +0900

    マウスの形を変化

--- sample_page/src/components/BodyContent.vue
+++ sample_page/src/components/BodyContent.vue
@@ -3,7 +3,7 @@
     <h1>Welcome to Mypage</h1>
     <hr />
 
-    <div @click="clickLink" style="font-size:120%;">{{link}}</div>
+    <div @click="clickLink" style="font-size:120%;cursor:pointer;">{{link}}</div>
     <span v-if="listLink" class="link-list">
       <span @mouseenter="qiitaShowIcon" @mouseleave="qiitaHiddenIcon" class="qiita-list">
         <a href="https://qiita.com/ritarock">qiita</a>
@@ -17,18 +17,18 @@
     </span>
 
     <span class="icon">
-      <span class="qiita-icon" v-if="qiitaActiveIcon"><img src="../assets/qiita.png" width="400" height="400"></span>
-      <span class="github-icon" v-if="githubActiveIcon"><img src="../assets/GitHub.png" width="400" height="400"></span>
-      <span class="hatenablog-icon" v-if="hatenablogActiveIcon"><img src="../assets/hatena.png" width="400" height="400"></span>
+      <span class="qiita-icon" v-if="qiitaActiveIcon"><img src="../assets/qiita.png"></span>
+      <span class="github-icon" v-if="githubActiveIcon"><img src="../assets/GitHub.png"></span>
+      <span class="hatenablog-icon" v-if="hatenablogActiveIcon"><img src="../assets/hatena.png"></span>
     </span>
 
-    <div @click="clickWho" class="who-list" style="font-size:120%;">{{who}}</div>
+    <div @click="clickWho" class="who-list" style="font-size:120%;cursor:pointer;">{{who}}</div>
     <span v-if="linkWho">
       <span class="who-name">NAME: ritarock</span>
       <span class="who-mail">MAIL: wkryz1204@gmail.com</span>
     </span>
 
-      <span id="footer"><a href="https://github.com/ritarock/sand-box/tree/master/sample_page">This page is made of vuejs</a></span>
+    <span id="footer"><a href="https://github.com/ritarock/sand-box/tree/master/sample_page">This page is made of vuejs</a></span>
 
   </div>
 </template>
@@ -124,18 +124,18 @@ h1{
 }
 .qiita-icon{
   position: absolute;
-  top: 100px;
-  left: 450px
+  top: 100;
+  left: 25%;
 }
 .github-icon{
   position: absolute;
-  top: 100px;
-  left: 450px
+  top: 100;
+  left: 25%;
 }
 .hatenablog-icon{
   position: absolute;
-  top: 120px;
-  left: 450px
+  top: 100;
+  left: 17%;
 }
 .who-list {
   position: absolute;
@@ -155,28 +155,28 @@ h1{
   font-size:120%;
 }
 body , html {
-    height: 100%;
+  height: 100%;
 }
 
 #container {
-    width: 100%;
-    position: relative;
-    height: auto !important;
-    height: 100%;
-    min-height: 100%;
+  width: 100%;
+  position: relative;
+  height: auto !important;
+  height: 100%;
+  min-height: 100%;
 }
 
 #contents {
-    padding-bottom: 100px;
+  padding-bottom: 100px;
 }
 #footer {
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-    height: 100px;
-    font-size: 85%;
-    font-weight: normal;
-    text-align: center;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 100px;
+  font-size: 85%;
+  font-weight: normal;
+  text-align: center;
 }
 
 </style>
