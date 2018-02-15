2dd0473050aba7e2d05fcaa5b8ef87bf1bc23107
ritarock <wkryz1204@gmail.com>
2018-02-15 22:14:24 +0900

    update

--- sample_page_docker/src/src/components/BodyContent.vue
+++ sample_page_docker/src/src/components/BodyContent.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="body">
-    <h1>Welcome to Mypage</h1>
+    <h1>Mypage</h1>
     <hr />
 
     <div @click="clickLink" style="font-size:120%;cursor:pointer;">{{link}}</div>
@@ -28,7 +28,7 @@
       <span class="who-mail">MAIL: wkryz1204@gmail.com</span>
     </span>
 
-      <span id="footer"><a href="https://github.com/ritarock/sand-box/tree/master/sample_page">This page is made of vuejs</a></span>
+    <span id="footer"><a href="https://github.com/ritarock/sand-box/tree/master/sample_page">This page is made of vuejs</a></span>
 
   </div>
 </template>
@@ -155,28 +155,26 @@ h1{
   font-size:120%;
 }
 body , html {
-    height: 100%;
+  height: 100%;
 }
-
 #container {
-    width: 100%;
-    position: relative;
-    height: auto !important;
-    height: 100%;
-    min-height: 100%;
+  width: 100%;
+  position: relative;
+  height: auto !important;
+  height: 100%;
+  min-height: 100%;
 }
-
 #contents {
-    padding-bottom: 100px;
+  padding-bottom: 100px;
 }
 #footer {
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-    height: 100px;
-    font-size: 85%;
-    font-weight: normal;
-    text-align: center;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 100px;
+  font-size: 85%;
+  font-weight: normal;
+  text-align: center;
 }
 
 </style>
