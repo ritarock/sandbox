68a684628d2a8064250ce6b03d1e5af34014a032
ritarock <dvryz1204@gmail.com>
2018-03-29 19:59:58 +0900

    雛形完成

--- sample_page_docker/src/App.vue
+++ sample_page_docker/src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <MainContent/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import MainContent from './components/MainContent'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainContent
   }
 }
 </script>
 
 <style>
-#app {
+/*#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
 </style>
