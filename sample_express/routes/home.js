7097bbce0758f5641c2ea5c9d5e3803a24508e59
ritarock <dvryz1204@gmail.com>
2018-09-20 00:15:25 +0900

    update

--- sample_express/routes/home.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.render('home', {title: 'TITLE', body: 'BODY'});
-});
-
-module.exports = router;
