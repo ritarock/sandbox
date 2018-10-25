7143cb898297c1594e48a7f199a821f0f0fc2475
ritarock <dvryz1204@gmail.com>
2018-10-26 00:59:03 +0900

    update

---
+++ my-page/my-page/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+    <div>
+      footer
+    </div>
+    );
+  }
+}
