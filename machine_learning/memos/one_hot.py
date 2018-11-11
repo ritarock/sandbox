159eaee32e6251dbc2d254a161c3e131e53e8435
rita <wkryz1204@gmail.com>
2018-11-11 15:43:45 +0900

    update

--- machine_learning/memos/one_hot.py
+++
@@ -1,8 +0,0 @@
-# one-hot変換関数
-def one_hot(target):
-   from sklearn import preprocessing
-   from sklearn.preprocessing import OneHotEncoder
-   target_vector = target.values
-   target_vector_enc = preprocessing.LabelEncoder().fit_transform(target_vector).reshape(-1,1)
-   profession_enc = OneHotEncoder().fit_transform(target_vector_enc).toarray()
-   return profession_enc
--- machine_learning/memos/standardization.py
+++
@@ -1,8 +0,0 @@
-# 標準化する関数
-def zscore(target):
-   target_mean = target.mean()
-   target_std = np.std(target)
-
-   zscore_point = (target - target_mean) / target_std
-
-   return zscore_point
