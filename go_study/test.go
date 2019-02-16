d436e9a7d507c1ef19b3ba653f2fdd9109dab92e
ritarock <dvryz1204@gmail.com>
2019-02-17 01:08:56 +0900

    update

--- go_study/test.go
+++ go_study/test.go
@@ -5,9 +5,15 @@ import (
 )
 
 func main() {
-	fruits := [3]string{"A", "B", "C"}
+	// A
+	s := make([]int, 0, 0)
+	fmt.Printf("(A) len=%d, cap=%d\n", len(s), cap(s))
 
-	for i, s := range fruits {
-		fmt.Printf("fruits[%d]=%s\n", i, s)
-	}
+	// B
+	s = append(s, 1)
+	fmt.Printf("(B) len=%d, cap=%d\n", len(s), cap(s))
+
+	// C
+	s = append(s, []int{2, 3, 4}...)
+	fmt.Printf("(C) len=%d, cap=%d\n", len(s), cap(s))
 }
