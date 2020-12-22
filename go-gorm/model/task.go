40e12d831e8ac37ed1801134300ee382fa791599
ritarock <dvryz1204@gmail.com>
2020-12-23 07:31:38 +0900

    create crud

--- go-gorm/model/task.go
+++ go-gorm/model/task.go
@@ -21,10 +21,43 @@ func (t Task) GetAll() []Task {
 	return tasks
 }
 
+func (t Task) FindId(id uint) Task {
+	db := GormConnect()
+	defer db.Close()
+
+	var task Task
+	db.Where("id = ?", id).Find(&task)
+	return task
+}
+
 func (t Task) Create() {
 	db := GormConnect()
 	defer db.Close()
 
 	var task = Task{Name: t.Name, Status: t.Status}
 	db.Create(&task)
+}
+
+func (t Task) Get() *gorm.DB {
+	db := GormConnect()
+	defer db.Close()
+
+	var task = Task{Name: t.Name}
+	return db.First(&task)
+}
+
+func (t Task) Update() {
+	db := GormConnect()
+	defer db.Close()
+
+	var task = Task{Name: t.Name}
+	db.Updates(&task)
+}
+
+func (t Task) Delete() {
+	db := GormConnect()
+	defer db.Close()
+
+	var task = Task{Model: gorm.Model{ID: t.ID}}
+	db.Delete(&task)
 }
