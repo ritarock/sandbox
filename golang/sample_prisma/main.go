2517b480898589a483e0adee9370e2d69f0b86d4
尾崎亮太 <dvryz1204@gmail.com>
2021-03-16 21:15:19 +0900

    init

---
+++ golang/sample_prisma/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"sample_prisma/prisma/db"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	client := db.NewClient()
+	if err := client.Prisma.Connect(); err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err := client.Prisma.Disconnect(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	r := gin.Default()
+
+	r.POST("/tasks", func(c *gin.Context) {
+		var task db.TaskModel
+		if err := c.ShouldBind(&task); err == nil {
+			c.String(http.StatusOK, `Bind err`)
+		}
+		var text *string
+		if newText, ok := task.Text(); ok {
+			text = &newText
+		}
+		var completed *bool
+		if newCompleted, ok := task.Completed(); ok {
+			completed = &newCompleted
+		}
+		newTask, err := client.Task.CreateOne(
+			db.Task.Text.SetIfPresent(text),
+			db.Task.Completed.SetIfPresent(completed),
+		).Exec(context.Background())
+		if err != nil {
+			fmt.Println(err)
+		}
+		c.JSON(http.StatusOK, newTask)
+	})
+
+	r.GET("/tasks", func(c *gin.Context) {
+		tasks, err := client.Task.FindMany().OrderBy(
+			db.Task.ID.Order(db.ASC),
+		).Exec(context.Background())
+		if err != nil {
+			fmt.Println(err)
+		}
+		c.JSON(http.StatusOK, tasks)
+	})
+
+	r.Run()
+}
