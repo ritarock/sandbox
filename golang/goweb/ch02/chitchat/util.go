de5d2aa3de52d9aeda9e667954b2f178ede207b8
ritarock <dvryz1204@gmail.com>
2021-04-03 18:17:06 +0900

    update

---
+++ golang/goweb/ch02/chitchat/util.go
@@ -0,0 +1,19 @@
+func session(w http.ResponseWriter, r *http.Request) (sess data.Session, err error) {
+	cookie, err := r.Cookie("_cookie")
+	if err == nil {
+		sess = data.Session(Uuid: cooke.Value)
+		if ok, _ := sess.Check(); !ok {
+			err = errors.New("Invalid session")
+		}
+	}
+	return
+}
+
+func generateHtml(w http.ResponseWriter, data interface{}, fn ...string) {
+	var files []string
+	for _, file := range fn {
+		files = append(files, fmt.Sprintf("templates/%s.html", file))
+	}
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(writer, "layout", data)
+}
