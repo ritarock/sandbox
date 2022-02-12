5506a6f5abb035069eade86653f4f67a4b577be3
ritarock <dvryz1204@gmail.com>
2022-02-12 18:11:26 +0900

    update

---
+++ golang/sample-ent-schema-edges/ent/schema/user.go
@@ -0,0 +1,33 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.Int("age"),
+		field.String("name"),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("card", Card.Type). // O2O
+						Unique(),
+
+		edge.To("pets", Pet.Type), // O2M
+
+		edge.From("groups", Group.Type). // M2M
+							Ref("users"),
+	}
+}
